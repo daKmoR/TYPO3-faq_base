488273437cf8315c9f485f904b781d6304ac5371
Thomas Allmer <d4kmor@gmail.com>
2013-04-22 11:40:01 +0200

    [FEATURE] proper language overlay handling

--- Classes/Domain/Model/Category.php
+++ Classes/Domain/Model/Category.php
@@ -40,8 +40,11 @@ class Category extends \TYPO3\CMS\Extbase\Domain\Model\Category {
 	public function getFaqs() {
 		$cleanedFaqs = array();
 //		$faqs = \TYPO3\CMS\Core\Category\Collection\CategoryCollection::load($this->getUid(), TRUE, 'tx_faqbase_domain_model_entry');
-
 		// @toDo: above function is buggy :/ fetch them manually
+
+		// fetch with default language
+		$where = 'AND sys_category.uid=' . intval($this->getUid()) . ' AND sys_category_record_mm.tablenames = "tx_faqbase_domain_model_entry" ' . ' AND tx_faqbase_domain_model_entry.sys_language_uid=0 ';
+		$where .= $GLOBALS['TSFE']->sys_page->enableFields('tx_faqbase_domain_model_entry');
 
 		$relatedRecords = array();
 		$resource = $GLOBALS['TYPO3_DB']->exec_SELECT_mm_query(
@@ -49,25 +52,21 @@ class Category extends \TYPO3\CMS\Extbase\Domain\Model\Category {
 			'sys_category',
 			'sys_category_record_mm',
 			'tx_faqbase_domain_model_entry',
-			'AND sys_category.uid=' . intval($this->getUid()) . ' AND sys_category_record_mm.tablenames = "tx_faqbase_domain_model_entry"' . ' AND tx_faqbase_domain_model_entry.sys_language_uid=' . $GLOBALS['TSFE']->sys_language_uid
+			$where
 		);
 
 		if ($resource) {
 			while ($record = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($resource)) {
+				// overlay language record if needed
+				if ($GLOBALS['TSFE']->sys_language_content !== 0) {
+					$record = $GLOBALS['TSFE']->sys_page->getRecordOverlay('tx_faqbase_domain_model_entry',  $record, $GLOBALS['TSFE']->sys_language_content,  $GLOBALS['TSFE']->sys_language_contentOL);
+				}
 				$relatedRecords[] = $record;
 			}
 			$GLOBALS['TYPO3_DB']->sql_free_result($resource);
 		}
 
-		$faqs = $relatedRecords;
-
-		foreach ($faqs as $faq) {
-			if ($faq['deleted'] === '0') {
-				$cleanedFaqs[] = $faq;
-			}
-		}
-
-		return $cleanedFaqs;
+		return $relatedRecords;
 	}
 
 }
